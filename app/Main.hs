256be837216ca44638f154aa02cda894ae45bf59
Akram Darwazeh <akram@invokear.com>
2020-04-06 23:31:44 +1200

    get api key from environment and begin setting up event handlers

--- app/Main.hs
+++ app/Main.hs
@@ -4,4 +4,4 @@ module Main where
 import Lib
 
 main :: IO ()
-main = someFunc
+main = botstart
--- src/Lib.hs
+++ src/Lib.hs
@@ -1,9 +1,26 @@
 module Lib
-    ( someFunc
+    ( botstart
     ) where
+import Control.Monad (when)
+import System.Environment (getEnv)
 import Discord
 import Discord.Types
 import qualified Discord.Requests as R
+import qualified Data.Text.IO as TIO
+import qualified Data.Text as T
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+botstart :: IO ()
+botstart = do 
+            token <- T.pack <$> (getEnv $ "DISCORD_CLIENT_SECRET")
+            userFacing <- runDiscord $ def 
+                            { discordToken = token
+                            , discordOnEvent = eventHandler
+                            , discordOnStart = handleStart }
+            TIO.putStrLn userFacing
+
+eventHandler :: DiscordHandle -> Event -> IO ()
+eventHandler handle event = case event of 
+    _ -> pure ()
+
+handleStart :: DiscordHandle -> IO ()
+handleStart handle = putStrLn "Bot started"
