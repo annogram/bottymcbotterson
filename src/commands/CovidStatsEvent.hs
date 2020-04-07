512d2b8e503066ed20362af39f894cce6d33f1cb
Akram Darwazeh <akram@invokear.com>
2020-04-08 09:58:57 +1200

    Add wreq for api calls

--- src/commands/CovidStatsEvent.hs
+++ src/commands/CovidStatsEvent.hs
@@ -5,11 +5,12 @@ module CovidStatsEvent
     ) where
 import Discord
 import Discord.Types
-import qualified Discord.Requests as R
 import Data.Text
+import qualified Discord.Requests as R
 
 covidStatsCommand :: Text
 covidStatsCommand = "/covid"
+
 -- Get information on the Covid-19 pandemic
 getCovidInfo :: DiscordHandle -> Event -> IO ()
 getCovidInfo handle (MessageCreate m) = do 
