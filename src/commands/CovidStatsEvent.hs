271c3269535efd2598f4bc2a79340cb96c65158d
Akram Darwazeh <akram@invokear.com>
2020-04-08 15:00:14 +1200

    Use aeson and Wreq to make an api call

--- src/commands/CovidStatsEvent.hs
+++ src/commands/CovidStatsEvent.hs
@@ -6,6 +6,9 @@ module CovidStatsEvent
 import Discord
 import Discord.Types
 import Data.Text
+import Data.Aeson.Lens
+import Network.Wreq
+import Control.Lens
 import qualified Discord.Requests as R
 
 covidStatsCommand :: Text
@@ -14,5 +17,15 @@ covidStatsCommand = "/covid"
 -- Get information on the Covid-19 pandemic
 getCovidInfo :: DiscordHandle -> Event -> IO ()
 getCovidInfo handle (MessageCreate m) = do 
-    _ <- restCall handle $  R.CreateMessage (messageChannel m) $ "Pong!"
-    pure ()
+    ip <- getInfo
+    _ <- restCall handle $  R.CreateMessage (messageChannel m) $ ip
+    return ()
+
+headerOpt :: Options
+headerOpt = defaults & header "Accept" .~ ["application/json"]
+
+getInfo :: IO (Text)
+getInfo = do 
+    r <- getWith headerOpt "http://httpbin.org/get"
+    let [v] = r ^.. responseBody . key "origin" . _String
+    return (v)
