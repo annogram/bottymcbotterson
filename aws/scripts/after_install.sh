82a1e14d3a9b47f95faa76e9bf5165a65232da9a
Akram Darwazeh <akram@invokear.com>
2020-05-09 14:16:31 +1200

    Use systemd directly

--- aws/scripts/after_install.sh
+++ aws/scripts/after_install.sh
@@ -6,8 +6,8 @@ LOG=$HOME/deploy.log
 /bin/echo "$(date '+%Y-%m-%d'): Changing owner and group of application... " >> $LOG
 
 # verify that the application directory has the correct owner/group
-/usr/bin/sudo /bin/chown -R ec2-user:ec2-user /home/ec2-user/botty_mc_botterson
-
+/usr/bin/sudo /bin/chown -R ubuntu:ubuntu /home/ubuntu/botty_mc_botterson
+/usr/bin/sudo systemctl botty.service restart
 echo -e "Done" >> $LOG
 
 /bin/echo "$(date '+%Y-%m-%d %X'): ** After Install Hook Completed **" >> $LOG
